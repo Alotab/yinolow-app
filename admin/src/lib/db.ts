1346bfd655b1dbd90b2b45c8ff29f93e298f8fa8
Albert Osei <alotab@yahoo.com>
2025-10-29 17:19:16 -0700

    database setup

--- admin/src/lib/db.ts
+++ admin/src/lib/db.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-const logger = require("../utils/logger")
+import { logger } from "../utils/logger";
+// const logger = require("../utils/logger")
 dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/reactmovie_admin";
