c4e76e05a20c93f8f0420116850954aaa9edeabb
Albert Osei <alotab@yahoo.com>
2025-10-29 17:18:34 -0700

    product.routes

--- admin/src/routes/product.routes.ts
+++ admin/src/routes/product.routes.ts
@@ -1,12 +1,15 @@
 // src/routes/product.routes.ts
 import { Router } from "express";
-import { createProduct, listProducts, getProduct } from "../controllers/product.controller";
+import { createProduct, listProducts, getProduct, updateProduct, deleteProduct } from "../controllers/product.controller";
 import { requireAuth, requireRole } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.get("/", listProducts);
 router.get("/:id", getProduct);
+router.get("/update", updateProduct);
+router.delete("/:id", deleteProduct);
+
 
 // admin-only create
 router.post("/", requireAuth, requireRole("admin"), createProduct);
