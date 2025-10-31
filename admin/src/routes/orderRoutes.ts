9a2dfdfaa54f5c5d65025d383834e3236edac30b
Albert Osei <alotab@yahoo.com>
2025-10-31 04:14:34 -0700

    orderRoutes

--- admin/src/routes/orderRoutes.ts
+++ admin/src/routes/orderRoutes.ts
@@ -1,11 +1,14 @@
 import express from "express";
-import { checkout } from "../controllers/orderController";
+import { checkout, getOrderByTrackingCode } from "../controllers/orderController";
 import { authMiddleware } from "../middlewares/authMiddleware";
+
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
+
 router.post("/checkout", checkout);
+router.get("/track/:trackingCode", getOrderByTrackingCode);  // Expose a public route for tracking Orders
 
 export default router;
