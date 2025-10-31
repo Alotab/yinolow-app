f8abc0e36eafdeb0f8dab05bfba61e5431e5fec2
Albert Osei <alotab@yahoo.com>
2025-10-31 04:14:56 -0700

    cartRoutes

--- admin/src/routes/cartRoutes.ts
+++ admin/src/routes/cartRoutes.ts
@@ -6,10 +6,10 @@ const router = express.Router();
 
 router.use(authMiddleware); // protect all routes
 
-router.post("/", cartControlller.addItem);
-router.post("/", cartControlller.getCart);
-router.delete("/:productId", cartControlller.removeItem);
-router.delete("/", cartControlller.clearCart);
+router.post("/", cartControlller.addItem);                      // Add item to cart
+router.get("/", cartControlller.getCart);                       // get user's cart
+router.delete("/:productId", cartControlller.removeItem);       // Remove one item 
+router.delete("/", cartControlller.clearCart);                  // clear entire cart
 
 
 export default router;
